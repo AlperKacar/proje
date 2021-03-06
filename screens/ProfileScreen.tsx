6977ec197ed6de52f4388a5469525995d042be8d
Alper <alper.kacar.3@gmail.com>
2021-03-06 11:03:31 +0300

    modal

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { StyleSheet, Text, View ,Image ,Dimensions} from 'react-native'
 const WIDTH = Dimensions.get('screen').width
-const PostScreen = (props) => {
+const ProfileScreen = (props) => {
     console.log(props)
+    const name = props.route.params.name
+    const image = props.route.params.url
 
     return (
         <View>
-            <Text>asd</Text>
-           
-
+           <Text>{name}</Text>
+     
+            <Image resizeMode={'contain'} style={{ width:WIDTH, height: 300}} source={{ uri: image }} /> 
         </View>
     )
 }
 
-export default PostScreen
+export default ProfileScreen
 
 const styles = StyleSheet.create({})
-
-
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,17 +1,18 @@
 import  React,{useState} from 'react';
-import { StyleSheet, TextInput, TouchableOpacity,Alert, Modal } from 'react-native';
-import { Text, View } from '../components/Themed';
+import { StyleSheet, TextInput, TouchableOpacity,Alert, Modal, Button, ScrollView, SafeAreaView, Text, View} from 'react-native';
+
 
 export default function TabTwoScreen(props) {
    
   const [name, setName] = useState("")
   const [url, setUrl] = useState() 
   const [visible, setVisible] = useState(false)
+  const [secondModalVısıble, setSecondModalVısıble] = useState(false)
 
-  const goToProfilescreen = ()=>{
+  const goToProfilescreen =   ( asdf:string,tttt:string)=>{
     setVisible(false)
-    props.navigation.navigate('PostSc',{name:name,url:url})
-
+    setSecondModalVısıble(true)
+    props.navigation.navigate('Prfsc',{name:asdf,url:tttt})
   }
 
   const login = () => {
@@ -20,34 +21,31 @@ export default function TabTwoScreen(props) {
 
  return(
    <View>
-    <View style={styles.urll}>
-      <Text style={{fontSize: 15,marginTop: 5,borderWidth: 1  }}>IMAGE URL</Text>
-     <TextInput value={url} onChangeText={(text) => setUrl(text)} style={{ color: '#3f734d',backgroundColor: 'white', fontSize: 20, borderWidth: 1, width: '70%', padding: 3, marginLeft:20}} />
-    </View>
     <View >
-      <TouchableOpacity onPress={() => setVisible(true)}  style={{ marginTop: 20, borderRadius: 10, backgroundColor: 'blue', height: 40, justifyContent: 'center', alignItems: 'center', width: '70%' ,marginLeft:60}}>
+      <TouchableOpacity  onPress={() => setVisible(true)} style={{ marginTop: 20, borderRadius: 10, backgroundColor: 'blue', height: 40, justifyContent: 'center', alignItems: 'center', width: '70%' ,marginLeft:60}}>
         <Text>User Information</Text>
       </TouchableOpacity>
     </View>
-    <Modal visible={visible}>
-      <View style={{flex: 1, backgroundColor:'red', alignItems:'center',justifyContent:'center'}}>
-        <Text>asddas</Text>
+    <Modal visible={visible} transparent onRequestClose={()=>setVisible(false)}>
+      <View style={{flex: 1, backgroundColor:'#000000aa', alignItems:'center',justifyContent:'center'}}>
+        <Text style={{marginBottom: 20,fontSize:20}}>NAME</Text>
         <TextInput value={name} onChangeText={(text) => setName(text)} style={{ color: '#3f734d',backgroundColor: 'white', fontSize: 20, borderWidth: 1, width: '70%', padding: 3, marginLeft:20}} />
-        <TouchableOpacity onPress={() => goToProfilescreen()}  style={{ marginTop: 20, borderRadius: 10, backgroundColor: 'blue', height: 40, justifyContent: 'center', alignItems: 'center', width: '70%' ,marginLeft:60}}>
-        <Text>go to profile</Text>
-      </TouchableOpacity>
+        <TouchableOpacity onPress={() => goToProfilescreen(name,'https://picsum.photos/300/300')}  style={{ marginTop: 20, borderRadius: 10, backgroundColor: 'blue', height: 40, justifyContent: 'center', alignItems: 'center', width: '70%' ,marginLeft:20}}>
+          <Text>go to profile</Text>
+        </TouchableOpacity>
       </View>
+    </Modal>
+    <Modal visible={secondModalVısıble} onRequestClose={()=>setSecondModalVısıble(false)}>
+        <View style={{backgroundColor:'red', flex:1}}> </View>
     </Modal>
    </View>
  )
 }
 const styles = StyleSheet.create({
   urll: {
-    flexDirection: 'row',
-    paddingHorizontal:5,
-    paddingVertical: 15,
+flex:1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
 
-  }
 });
